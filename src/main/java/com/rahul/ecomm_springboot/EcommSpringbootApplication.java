8465b7216abc13a6866527fef445beca5a2dec00
Rahul <rsc120@nist.edu>
2025-04-18 12:16:09 +0530

    ecomm

--- src/main/java/com/rahul/ecomm_springboot/EcommSpringbootApplication.java
+++ src/main/java/com/rahul/ecomm_springboot/EcommSpringbootApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EcommSpringbootApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EcommSpringbootApplication.class, args);
 	}
-
 }
